ca620fc9ade8273da763cc596e39cd8c6adddd3e
Iivari Karrila <iivari.karrila@tuni.fi>
2023-01-25 14:09:30 +0200

    Temperature task

--- student/02/temperature/main.cpp
+++ student/02/temperature/main.cpp
@@ -4,9 +4,18 @@ using namespace std;
 
 int main()
 {
-    cout << "Enter a temperature: ";
+    float temp;
+    float celsius;
+    float fahrenheit;
 
-    // Write your code here
+    cout << "Enter a temperature: ";
+    cin >> temp;
+
+    fahrenheit = (temp * 1.8 + 32);
+    celsius = ((temp - 32) / 1.8);
+
+    cout << temp << " degrees Celsius is " << fahrenheit << " degrees Fahrenheit" << endl;
+    cout << temp << " degrees Fahrenheit is " << celsius << " degrees Celsius" << endl;
 
     return 0;
 }
