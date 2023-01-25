1ac79cf37e96a5a51f4d91a1d2ef42240724422e
Iivari Karrila <iivari.karrila@tuni.fi>
2023-01-25 16:00:38 +0200

    Lisätty myös 7:llä jaollisten lukujen käsittely

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -9,9 +9,13 @@ int main()
     std::cout << "How many numbers would you like to have? ";
     std::cin >> amount;
 
-    while ( i <= amount ){
-        if ( i % 3 == 0 ){
+    while (i <= amount){
+        if ((i % 3 == 0) && (i % 7 == 0)){
+            std::cout << "zip boing" << std::endl;
+        } else if (i % 3 == 0){
             std::cout << "zip" << std::endl;
+        } else if (i % 7 == 0){
+            std::cout << "boing" << std::endl;
         } else{
             std::cout << i << std::endl;
         }
