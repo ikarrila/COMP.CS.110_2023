341e9e45162549eb8c68e80b86ba0cfd18d5c0f4
Iivari Karrila <iivari.karrila@tuni.fi>
2023-01-25 14:32:23 +0200

    Syötteen lukeminen ja numeroiden tulostus

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -3,9 +3,16 @@
 
 int main()
 {
-    std::cout << "How many numbers would you like to have? ";
+    int i = 0;
+    int amount;
 
+    std::cout << "How many numbers would you like to have? ";
+    std::cin >> amount;
     // Write your code here
+    while ( i <= amount ){
+        std::cout << i << std::endl;
+        ++i;
+    }
 
     return 0;
 }
