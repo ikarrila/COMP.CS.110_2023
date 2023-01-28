5a944e0f145fe7b4673c5b10acd2a9b98e1d6919
Iivari Karrila <iivari.karrila@tuni.fi>
2023-01-28 22:02:28 +0200

    Encryption

--- student/02/encryption/main.cpp
+++ student/02/encryption/main.cpp
@@ -1,34 +1,84 @@
 #include <iostream>
+#include <cctype>
+#include <string>
 
 using namespace std;
+std::string az = "abcdefghijklmnopqrstuvwxyz";
+
+int checkWord(std::string input){
+    string::size_type len = 0;
+    len = input.length();
+    for (unsigned long i = 0; i < len; i++){
+        if ( input[i] >= 'a' && input[i] <= 'z' ){
+            //cout << "Char " << input[i] << " found." << endl;
+        } else {
+            cout << "Error! The text to be encrypted must contain only lower case characters." << endl;
+            exit(EXIT_FAILURE);
+        }
+    }
+    return 0;
+}
+
+int checkChar(std::string input){
+    string::size_type len = 0;
+    len = input.length();
+    for (unsigned long i = 0; i < len; i++){
+        if ( input[i] >= 'a' && input[i] <= 'z' ){
+            //cout << "Char " << input[i] << " found." << endl;
+        } else {
+            cout << "Error! The encryption key must contain only lower case characters." << endl;
+            exit(EXIT_FAILURE);
+        }
+    }
+    return 0;
+}
+
+int findAll(std::string input){
+    string::size_type len = 0;
+    len = input.length();
+    for (unsigned long i = 0; i < len; i++){
+        if ( input.find(az[i]) == string::npos ){
+            cout << "Error! The encryption key must contain all alphabets a-z." << endl;
+            exit(EXIT_FAILURE);
+        }
+    }
+    return 0;
+}
+
+int encryption(std::string word, std::string key){
+    string::size_type len = 0;
+    len = word.length();
+    string empty = "";
+    for (unsigned long i = 0; i < len; i++){
+        int index = az.find(word[i]);
+        empty.insert(i, 1, key[index]);
+    }
+    cout << "Encrypted text: " << empty << endl;
+    return 0;
+}
 
 int main()
 {
-    string key;
-    string allLetters = "abcdefghijklmnopqrstuvwxyz";
+    string key = "";
+    string word = "";
+
     cout << "Enter the encryption key: ";
     cin >> key;
 
-    if (key.length() < 26){
+    //Tarkistaa merkkijonon pituuden
+    if ( key.length() != 26 ) {
         cout << "Error! The encryption key must contain 26 characters." << endl;
-        return 1;
-    } else if (key.find_first_not_of(allLetters) ){
-        cout << "Error! The encryption key must contain only lower case characters." << endl;
-        return 1;
-    } else{
-        for (int i = 0; i < int(key.size()); ++i){
-            string::size_type finder = 0;
-            finder = key.find(allLetters[i]);
-            if ( finder == string::npos){
-                cout << "Error! The encryption key must contain all alphabets a-z." << endl;
-                return 1;
-            }
-        }
+        exit(EXIT_FAILURE);
     }
-        /*else if (key.find()){
-        cout << "Error! The encryption key must contain all alphabets a-z." << endl;
-    }*/
 
+    //Checks for all letters a-z, only small letters
+    checkChar(key);
+    findAll(key);
+
+    cout << "Enter the text to be encrypted: ";
+    cin >> word;
+    checkWord(word);
+    encryption(word, key);
 
     return 0;
 }
