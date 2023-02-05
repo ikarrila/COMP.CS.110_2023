bd6a9e99f854a282c91d06ed8a59618c6f2a7a82
Iivari Karrila <iivari.karrila@tuni.fi>
2023-02-05 23:19:39 +0200

    Split solved

--- student/04/split/main.cpp
+++ student/04/split/main.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <sstream>
+#include <algorithm>
 
 // TODO: Implement split function here
 // Do not change main function
+std::vector< std::string > split(std::string str, char separator, bool skip_empty = false){
+    std::stringstream test(str);
+    std::string segment;
+    std::vector<std::string> result;
 
+    while(std::getline(test, segment, separator)){
+        if (not skip_empty){
+            result.push_back(segment);
+        } else {
+            if (segment != ""){
+                result.push_back(segment);
+            }
+        }
+    }
+    if (str.back() == separator and skip_empty == false){
+        result.push_back((""));
+    }
+    return result;
+}
 
 int main()
 {
