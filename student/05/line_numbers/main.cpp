5e9fb7a5d8a0f49913dd5022cc1c9888c06db866
Iivari Karrila <iivari.karrila@tuni.fi>
2023-02-13 21:46:43 +0200

    Solved line_numbers

---
+++ student/05/line_numbers/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    string input_file = "";
+    string output_file = "";
+
+    cout << "Input file: ";
+    getline(cin, input_file);
+    cout << "Output file: ";
+    getline(cin, output_file);
+
+    ofstream output;
+    ifstream file_object(input_file);
+    output.open (output_file);
+    if( not file_object ){
+        cout << "Error! The file " << input_file << " cannot be opened." << endl;
+        return EXIT_FAILURE;
+    } else {
+        int row_number = 1;
+        string line;
+        while( getline(file_object, line) ){
+            output << row_number << " " << line << endl;
+            row_number += 1;
+        }
+        file_object.close();
+        output.close();
+    }
+    return EXIT_SUCCESS;
+}
