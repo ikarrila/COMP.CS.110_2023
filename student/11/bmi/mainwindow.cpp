6952791a4d99d06205db32d6cd905d418c0767e1
Iivari Karrila <iivari.karrila@tuni.fi>
2023-04-12 18:14:10 +0300

    Bmi calculator solved

---
+++ student/11/bmi/mainwindow.cpp
@@ -0,0 +1,39 @@
+#include "mainwindow.hh"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_countButton_clicked()
+{
+    weight_ = ui->weightLineEdit->text().toDouble();
+    height_ = ui->heightLineEdit->text().toDouble();
+
+    bmi_ = weight_ / ( height_ * height_ ) * 10000;
+
+    ui->resultLabel->setText(QString::number(bmi_));
+    print_weight_info(bmi_);
+}
+
+void MainWindow::print_weight_info(double bmi)
+{
+    if (bmi > 25)
+    {
+        ui->infoTextBrowser->setText("You are overweight.");
+    } else if (bmi < 18.5)
+    {
+        ui->infoTextBrowser->setText("You are underweight.");
+    } else
+    {
+        ui->infoTextBrowser->setText("Your weight is normal.");
+    }
+}
